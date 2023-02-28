f96a8d6b06e6d46c72563a5a778f670976bfcba7
Iyan Theekshana <iyan.dev123@gmail.com>
2023-02-28 09:16:24 +0100

    v-if, v-else-if, v-else, v-show, v-for

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,16 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goals: [],
+      enteredGoalValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = "";
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
